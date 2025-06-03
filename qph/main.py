1dd8cb5d0c98d8211fc387ee0efa0a2344f221e5
Ramis <ramis@Ramiss-Air.Dlink>
2025-06-03 23:53:40 +0300

    QARertiever -> chain

--- qph/main.py
+++ qph/main.py
@@ -2,9 +2,9 @@ import os
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.chains.retrieval_qa.base import RetrievalQA
-# from langchain_ollama import OllamaLLM
+from langchain_core.output_parsers import StrOutputParser
 from langchain_community.llms.llamacpp import LlamaCpp
+
 
 from vector_storage.storage import VectorStorage
 from qph.settings import settings
@@ -18,30 +18,25 @@ def main():
         storage = VectorStorage()
         storage.vectorise()
 
-    PROMPT = PromptTemplate(
-        input_variables=["context", "question"],
-        template="""
-            Ты — интеллектуальный помощник, который отвечает на вопросы, используя предоставленный контекст.
-            Контекст:
-            {context}
-            Вопрос: {question}
-            Ответ только на русском языке:
-            """
-    )
-
     embeddings = HuggingFaceEmbeddings(model_name=settings.MODEL_NAME)
     vectorstorage = FAISS.load_local(
         settings.FAISS_INDEX_PATH,
         embeddings,
         allow_dangerous_deserialization=True
     )
-
-    # llm = OllamaLLM(
-    #     model="mistral",
-    #     temperature=0.5,
-    #     num_predict=150,
-    #     verbose=False
-    # )
+    PROMPT = PromptTemplate(
+        input_variables=["context", "question"],
+        template="""
+        Ты — интеллектуальный помощник, который отвечает на вопросы, используя предоставленный контекст.
+        Контекст:
+        {context}
+        Вопрос:
+        {question}
+        Если в контексте достаточно информации, дай подробный и развернутый ответ.
+        Если в вопросе требуется указать количество или число, посчитай и дай точный ответ.
+        Ответ должен быть на русском языке.
+    """
+    )
 
     llm = LlamaCpp(
         model_path="gguf_models/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
@@ -49,27 +44,27 @@ def main():
         temperature=0.5,
         verbose=False,
         n_threads=8,
-        n_gpu_layers=32,
-        n_batch=128
+        n_gpu_layers=16,
+        n_batch=128,
+        max_tokens=256,
+        repeat_penalty=1.1,
     )
-
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        retriever=vectorstorage.as_retriever(search_kwargs={"k": 3}),
-        return_source_documents=True,
-        chain_type_kwargs={"prompt": PROMPT}
-    )
+    retriever = vectorstorage.as_retriever(search_kwargs={"k": 3})
+    parser = StrOutputParser()
+    chain = PROMPT | llm | parser
     try:
         while True:
             query = input("\nВаш вопрос: ")
             if not query:
                 break
-            result = qa.invoke({"query": query})
-            print("\nОтвет:", result["result"])
+            docs = retriever.invoke(query)
+            context = "\n\n".join([doc.page_content for doc in docs])
+            result = chain.invoke({"context": context, "question": query})
+            print("\nОтвет:", result)
             logger.info("\nНайденные документы:")
-            for doc in result["source_documents"]:
+            for doc in docs:
                 source = doc.metadata.get("source")
-                logger.info(f"[{source}] {doc.page_content[:100]}...")
+                logger.info(f"[{source}] {doc.page_content[:50]}...")
     except KeyboardInterrupt:
         print("\nBYE", flush=True)
 
