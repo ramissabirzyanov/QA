61df15759864eacc5472d4b0a113f629fc500de8
Ramis <ramis@Ramiss-Air.Dlink>
2025-06-09 12:11:08 +0300

    updare entry

--- assistant_app/assistant_core/main.py
+++ assistant_app/assistant_core/main.py
@@ -58,4 +58,4 @@ app.include_router(api_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("assistant_app.assistant_core.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
